2498db219052eac498a1f224d8b488d8966583fe
JhonEnFl <enriquezjhon321@gmail.com>
2025-02-28 22:40:46 -0500

    Seguridad y protección de datos mejorada

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const auth = require('../middleware/authMiddleware');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 
 const router = express.Router();
 
 /// Register user ///
-router.post("/register", async (req, res) => {
+router.post("/register", [body('email').isEmail().normalizeEmail(), body('password').isLength({min:8}).trim().escape()], async (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array()});
 
+    const { username, email, password } = req.body;
     const existUser = await User.findOne({ email });
     if (existUser) return res.status(400).json({ error: "User already exist"});
-
+    
     const newUser = new User({ username, email, password});
     await newUser.save();
     res.status(201).json({ message: "User created succesfully" });
 
   } catch (error) {
+    console.log(error.message);
     res.status(500).json({ error: "Register user failed" });
   }
 });
--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,30 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
 const postRoutes = require('./routes/post');
 const commentRoutes = require('./routes/comment');
 const userRoutes = require('./routes/user');
 
 dotenv.config();
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: { error: "Many request, try later." }
+});
+app.use('/auth/login', rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 5
+}));
 
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
+app.use(mongoSanitize());
 
 connectDB();
-
 
 app.use("/auth", authRoutes);
 app.use('/posts', postRoutes);
